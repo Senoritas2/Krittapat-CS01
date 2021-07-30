839cfe98bb3027161f7cc219196d7245be4ad853
Pooh <krittapattragoolpua@gmail.com>
2021-07-30 09:14:54 +0700

    CS01-11

---
+++ test.py
@@ -0,0 +1,36 @@
+#Neural Network Classification
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.cross_validation import train_test_split
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+from keras.utils import np_utils
+
+iris = sns.load_dataset('iris')
+sns.pairplot(iris, hue='species')
+
+X=iris.values[:,:4]
+y=iris.values[:,4]
+X_train,X_test,y_train,y_test=train_test_split(X,y,train_size=0.5,random_state=1)
+
+def one_hot_encode_object_array(arr):
+    uniques, ids = np.unique(arr, return_inverse=True)
+    return np_utils.to_categorical(ids, len(uniques))
+'''One hot encode a numpy array of objects'''
+   
+y_train_ohe=one_hot_encode_object_array(y_train)
+y_test_ohe=one_hot_encode_object_array(y_test)
+
+model=Sequential()
+model.add(Dense(16,input_shape=(4,)))
+model.add(Activation('sigmoid'))
+model.add(Dense(3))
+model.add(Activation('softmax'))
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+
+model.summary()
+
+model.fit(X_train,y_train_ohe,nb_epoch=20,batch_size=1,verbose=1)
+loss, accuracy = model.evaluate(X_test, y_test_ohe, verbose=1)
+print('The accuracy of the Neural Network classifier on test data is = {:.2f}'.format(accuracy))
