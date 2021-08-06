72bd4a5fa8346f3ba22734e329c3e28daee56c23
Pooh <krittapattragoolpua@gmail.com>
2021-08-06 09:57:44 +0700

    Update CS01-14.py

--- CS01-14.py
+++ CS01-14.py
@@ -1,7 +1,7 @@
-numlist = ['5','10','15','20','25','50','20']
+numlist = [5,10,15,20,25,50,20]
 
 for i in range(len(numlist)):
     print(i)
-    if (numlist[i] == '20'):
-        numlist[i] = '200'
+    if (numlist[i] == 20):
+        numlist[i] = 200
     print(numlist)
