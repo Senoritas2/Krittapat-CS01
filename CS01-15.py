72748593eea0f505bbf92a71e0ed77f5971fae7c
Pooh <krittapattragoolpua@gmail.com>
2021-08-20 09:22:43 +0700

    CS01-15

---
+++ CS01-15.py
@@ -0,0 +1,17 @@
+numlist = [5,10,15,20,25,50,20]
+
+def haha(list):
+    for i in range(len(numlist)):
+        print(i)
+        if (numlist[i] == 20):
+            numlist[i] = 200
+        print(numlist)
+haha(list)
+        
+        
+    
+    
+    
+
+    
+
